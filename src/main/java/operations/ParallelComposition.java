fbfb46a84383b593c1076634df719209f23ee9e9
Sebastian <nospam>
2014-10-30 14:32:22 +0100

    Interfaces for parallel-operator and utility-class.

---
+++ src/main/java/operations/ParallelComposition.java
@@ -0,0 +1,32 @@
+package operations;
+
+import java.util.Collection;
+import java.util.Set;
+
+import beans.Lts;
+
+
+/**
+ * Realization of the Parallel-Execution-Operator aka ||.
+ * 
+ * @author Sebastian
+ *
+ */
+public class ParallelComposition
+{
+  public static Lts intersect(Collection<Lts> automata)
+  {
+    // TODO implementation
+
+    return null;
+  }
+
+
+  public static Lts intersectByActions(Collection<Lts> automata, Set<String> actions)
+  {
+    // TODO implementation, no priority (not part of the task?)
+
+    return null;
+  }
+
+}
---
+++ src/main/java/operations/StringHelper.java
@@ -0,0 +1,6 @@
+package operations;
+
+public class StringHelper
+{
+
+}
