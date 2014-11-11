2982f22580c3bcea033359af1e86681eb8ed1867
singerm <still-not-dead@gmx.net>
2014-11-11 12:19:34 +0100

    mimimi

--- src/main/java/ctrl/Starter.java
+++ src/main/java/ctrl/Starter.java
@@ -1,15 +1,12 @@
 package ctrl;
 
 import java.io.File;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.swing.JFileChooser;
 
 import operations.ParallelComposition;
 import beans.Lts;
-import beans.Transition;
 
 public class Starter {
 
@@ -21,44 +18,25 @@ public class Starter {
 			chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
 			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 			chooser.setAcceptAllFileFilterUsed(false);
+			chooser.setDialogTitle("Bitte einen Ordner auswählen!");
 			// Dialog zum Oeffnen von Dateien anzeigen
 
-			if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			if (chooser.showDialog(null, "Ordner wählen") == JFileChooser.APPROVE_OPTION) {
 				folder = chooser.getSelectedFile();
 			}
 
-			List<Lts> allLts = Reader.read(folder, false);
+			if (folder != null) {
+				List<Lts> allLts = Reader.read(folder, false);
 
-			ParallelComposition composition = new ParallelComposition();
+				ParallelComposition composition = new ParallelComposition();
 
-			Lts x = composition.compute(allLts, getParallelActions(allLts));
-			Visualizer.visualize(x);
+				Lts x = composition.compute(allLts);
+				Visualizer.visualize(x);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
 
-	private static Set<String> getParallelActions(List<Lts> allLts) {
-		Set<String> parallelActions = new HashSet<String>();
-		Set<String> allActions = new HashSet<String>();
-		for (Lts lts : allLts) {
-			Set<String> worked = new HashSet<String>();
-			for (Transition trans : lts.getAllTransitions()) {
-
-				if (allActions.contains(trans.name)
-						&& !worked.contains(trans.name)) {
-					parallelActions.add(trans.name);
-				}
-
-				allActions.add(trans.name);
-				worked.add(trans.name);
-
-			}
-
-		}
-
-		return parallelActions;
-
-	}
 }
