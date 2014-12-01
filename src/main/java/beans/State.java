60953383361e25c2e4a9bbd3477881c384e75f80
singerm <still-not-dead@gmx.net>
2014-12-01 21:28:50 +0100

    added tarjan and better version for previous

--- src/main/java/beans/State.java
+++ src/main/java/beans/State.java
@@ -5,66 +5,59 @@ import java.util.Set;
 
 import beans.ctl.CtlFormula;
 
-
 /**
- * Zustand eines LTS. Von einem Zustand aus können mehrere Transitionen (Zustandsübergänge) zu anderen Zuständen ausgehen. Da es sich
- * hierbei das Äquivalent zu einem DEA handelt, kann eine Transition mit dem gleichen Namen nur einmal von einem Zustand ausgehen.
+ * Zustand eines LTS. Von einem Zustand aus können mehrere Transitionen
+ * (Zustandsübergänge) zu anderen Zuständen ausgehen. Da es sich hierbei das
+ * Äquivalent zu einem DEA handelt, kann eine Transition mit dem gleichen Namen
+ * nur einmal von einem Zustand ausgehen.
  * 
  * @author Dagon
  * 
  */
-public class State
-{
+public class State {
 
-  public String name = "";
-  public Set<Proposition> props = new HashSet<Proposition>();
-  public Set<CtlFormula> formulas = new HashSet<CtlFormula>();
+	public String name = "";
+	public Set<Proposition> props = new HashSet<Proposition>();
+	public Set<CtlFormula> formulas = new HashSet<CtlFormula>();
+	public Integer deapthIndex = null;
+	public int lowlink = 0;
+	public Set<State> prevStates = new HashSet<State>();
 
-  // In diesem Fall wird ein Set genutzt, da dies sicherstellt, da jeder
-  // Transition-Name nur einmal verwendet wird.
-  public Set<Transition> transitions = new HashSet<Transition>();
+	// In diesem Fall wird ein Set genutzt, da dies sicherstellt, da jeder
+	// Transition-Name nur einmal verwendet wird.
+	public Set<Transition> transitions = new HashSet<Transition>();
 
+	public State() {
+		super();
+	}
 
-  public State()
-  {
-    super();
-  }
+	public State(String name) {
+		this.name = name;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
-  public State(String name)
-  {
-    this.name = name;
-  }
-
-
-  @Override
-  public boolean equals(Object obj)
-  {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    State other = (State) obj;
-    if (name == null)
-    {
-      if (other.name != null)
-        return false;
-    }
-    else if (!name.equals(other.name))
-      return false;
-    return true;
-  }
-
-
-  @Override
-  public int hashCode()
-  {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((name == null) ? 0 : name.hashCode());
-    return result;
-  }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
 }
--- src/main/java/util/LtsHelper.java
+++ src/main/java/util/LtsHelper.java
@@ -2,6 +2,7 @@ package util;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 import beans.Lts;
 import beans.State;
@@ -9,19 +10,61 @@ import beans.Transition;
 
 public class LtsHelper {
 
-	public static Set<State> getPreviousStates(Lts lts, State state) {
-		Set<State> prevs = new HashSet<State>();
+	private static Stack<State> stack = new Stack<State>();
+	private static int depathIndex = 0;
+	private static Set<Set<State>> allScc = new HashSet<Set<State>>();
 
-		for (State possiblePrev : lts.getStates()) {
+	public static void setPreviouseStates(Lts lts) {
+		for (State s : lts.getStates()) {
 
-			for (Transition trans : possiblePrev.transitions) {
-				if (trans.followState.equals(state)) {
-					prevs.add(possiblePrev);
-				}
+			for (Transition trans : s.transitions) {
+
+				trans.followState.prevStates.add(s);
+
 			}
 
 		}
-		return prevs;
+
+	}
+
+	public static Set<Set<State>> getSCC(Lts lts) {
+		Set<State> input = lts.getStates();
+		depathIndex = 0;
+		allScc.clear();
+		stack.clear();
+		for (State s : input) {
+			if (s.deapthIndex == null) {
+				strongConnected(s);
+			}
+		}
+
+		return allScc;
+	}
+
+	private static void strongConnected(State s) {
+		s.deapthIndex = depathIndex;
+		s.lowlink = depathIndex;
+		depathIndex++;
+		stack.push(s);
+
+		for (Transition trans : s.transitions) {
+
+			if (trans.followState.deapthIndex == null) {
+				strongConnected(trans.followState);
+				s.lowlink = Math.min(s.lowlink, trans.followState.deapthIndex);
+
+			}
+		}
+
+		if (s.lowlink == s.deapthIndex) {
+			Set<State> scc = new HashSet<State>();
+			State w = null;
+			do {
+				w = stack.pop();
+				scc.add(w);
+			} while (s.equals(w));
+			allScc.add(scc);
+		}
 
 	}
 }
