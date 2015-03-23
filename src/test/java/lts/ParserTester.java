686bfe614fd4835101f88c9b74531c0895042c0b
singerm <still-not-dead@gmx.net>
2015-03-23 19:07:37 +0100

    tesdt

---
+++ src/test/java/lts/ParserTester.java
@@ -0,0 +1,14 @@
+package lts;
+
+import util.InputParser;
+
+public class ParserTester {
+
+	public static void main(String... args) {
+
+		String input = "NOT(E[a U b])";
+
+		InputParser.parseInput(input);
+
+	}
+}
